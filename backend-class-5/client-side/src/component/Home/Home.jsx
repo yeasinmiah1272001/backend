504ee181d36c1cbf6805ea1c33a36d0ccb136888
unknown <you@example.com>
2024-06-01 08:43:20 +0600

    d

---
+++ backend-class-5/client-side/src/component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../Banner/Banner";
+import TeaGallary from "../TeaGallary/TeaGallary";
+
+
+const Home = () => {
+       return (
+              <div>
+                     <Banner></Banner>
+                     <TeaGallary></TeaGallary>
+                     
+              </div>
+       );
+};
+
+export default Home;
